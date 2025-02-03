a22e0e0f27bdf7a6e7b63c892f35b32683c1821f
Florian <f.guillemard@alternadom.com>
2025-02-03 14:26:03 +0000

    Dont forget to update the script

--- install.sh
+++ install.sh
@@ -6,14 +6,15 @@ if [ -d "./ifpshare-converter" ]; then
     echo "Répertoire trouvé. Mise à jour en cours..."
     cd ./ifpshare-converter
     git pull
-    cd ..
 else
     echo "Répertoire non trouvé. Clonage du projet..."
     git clone https://github.com/KMChris/ifpshare-converter.git
     cd ./ifpshare-converter
-    sed -i "s/app\.run(debug=False)/app.run(host='0.0.0.0', port=5000, debug=False)/" app.py
-    cd ..
 fi
+
+# Modification du script app.py original
+sed -i "s/app\.run(debug=False)/app.run(host='0.0.0.0', port=5000, debug=False)/" app.py
+cd ..
 
 # Construction et déploiement
 echo "Construction et déploiement en cours..."
